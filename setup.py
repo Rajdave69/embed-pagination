364569f49cfe2b7313d8010318cb14e80f04b7f1
Raj Dave <10864@bpsdoha.edu.qa>
2023-04-07 22:02:26 +0300

    minor updates

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="embed-pagination",
-    version="1.0.0",
+    name="simple-embed-pagination",
+    version="1.0.2",
     description="Easily create pagination for discord.py embeds.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     py_modules=["paginator"],
     package_dir={'': "src"},
-    url="https://github.com/FaddyManatee/embed-pagination",
-    author="FaddyManatee",
+    url="https://github.com/RajDave69/embed-pagination",
+    authors=["FaddyManatee", "RajDave69"],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
